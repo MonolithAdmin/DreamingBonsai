fefa8e4563e523429df3438e3ab4b7ba1b59d2e5
MonolithAdmin <37859695+MonolithAdmin@users.noreply.github.com>
2026-04-04 18:49:07 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// Necessary for __dirname to work in modern ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  logLevel: 'error', 
+  logLevel: 'error',
   plugins: [
-    react(),
+    react()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  ],
   build: {
     outDir: 'dist',
-    // This ensures assets are linked correctly for cPanel subfolders if needed
+    // This ensures assets are linked correctly for cPanel subfolders
     assetsDir: 'assets',
   }
 })
